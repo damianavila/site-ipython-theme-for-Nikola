984e95f186727140539d0f7f7b867467244346ae
damianavila <damianavila82@yahoo.com.ar>
2013-01-27 22:12:09 -0300

    Fixed Mathjax bug and css looking like a real notebook.

---
+++ site-ipython/assets/js/mathjax-config.js
@@ -0,0 +1,10 @@
+MathJax.Hub.Config({
+    tex2jax: {
+        inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+        displayMath: [ ['$$','$$'], ["\\[","\\]"] ]
+    },
+    displayAlign: 'left', // Change this to 'center' to center equations.
+    "HTML-CSS": {
+        styles: {'.MathJax_Display': {"margin": 0}}
+    }
+});
